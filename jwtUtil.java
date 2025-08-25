4eac66ac84b244cc9ce12829f2c70ba5c15102e8
Abhinavnunna7 <abhinavnunna77@gmail.com>
2025-08-25 14:53:25 +0530

    Create jwtUtil.java

---
+++ jwtUtil.java
@@ -0,0 +1,33 @@
+package com.creatorconnect.service;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private String secret = "secretKeyForJWT";
+    private long expiry = 1000 * 60 * 60; // 1 hr
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiry))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
